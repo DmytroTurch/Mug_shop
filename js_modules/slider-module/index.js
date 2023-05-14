c397c4a768d3bec9b499f05d7c41c640dd3e1c03
DmytroTurch <dmturj@gmail.com>
2023-05-14 12:02:29 +0300

    Slider: update slider obj

--- js_modules/slider-module/index.js
+++ js_modules/slider-module/index.js
@@ -5,37 +5,201 @@ const thumbMinEl = document.querySelector('.thumb_min');
 const thumbMaxEl = document.querySelector('.thumb_max');
 
 const slider = {
-    width : 100,
+    ID: 0,
 
-    emptyMin: {
-        scale: 0,
-        translate: 0
-    },
-    mainSector: {
-        scale: 1,
-        translate: 0
-    },
-    emptyMax: {
-        scale: 0,
-        translate: 0,
+    thumbWidth: 10,
+    thumbHeight: 14,
+
+    trackWidth : undefined,
+    trackStart: undefined,
+    trackEnd: undefined,
+
+    changeIDto(newID) {
+        this.ID = newID;
     },
 
+    setDefaultStyles() {
+        document.getElementsByTagName('body')[0].innerHTML += `
+        <style>
+            .slider{
+                margin: 0;
+                border: 1px solid #c42525;
+            }
+            .slider__track {
+                height: 10px;
+                position: relative;
+                margin: 0;
+            }
 
-    // in further update make thumb realization in JS as array of standardized objs 
-    thumbMin: {
-        positionDefault: -50,
-        positionCurrent: -50, 
+            .sector:nth-child(even){
+                background-color: #858282;
+            }
+
+            .sector:nth-child(odd){
+                background-color: #000;
+            }
+
+            .thumb {
+                position: absolute;
+                top: 7px;
+                width: ${this.thumbWidth}px;
+                height: ${this.thumbHeight}px;
+                background-color: #bdb4b4;
+                border-radius: 5px;
+                box-shadow: inset 0.5px 0.5px 0.5px #000, inset -0.5px -0.5px 0.5px #000;
+            }
+        </style>
+        `;
     },
 
-    thumbMax: {
-       
+    renderSliderIn(targetElement, trackWidth, number) {
+        this.setDefaultStyles();
+        targetElement.innerHTML += `
+            <div class="slider-container" >
+                <style scoped>
+                    div#slider${this.ID} {
+                        width: ${trackWidth}px;
+                    }
+                    div#track${this.ID} {
+                        width: ${trackWidth}px;
+                    }
+                </style>
+
+                <div class="slider" id="slider${this.ID}">
+                    <div class="slider__track" id="track${this.ID}"></div>
+                </div>
+            </div>`;
+        this.trackWidth = trackWidth;
+        this.setTrackPosition(this.ID);
+        this.createThumbs(number);
+        this.setSectors();
+        this.calculateInitScale();
+        this.calculateInitTranslate();
+        this.setThumb('all');
+
+        console.log(`NumThumbs: ${this.thumb.length}`)
+        console.log(`NumThumbs: ${this.sector.length}`)
     },
+
+    setTrackPosition() {
+        let coordObj = document.getElementById(`track${this.ID}`).getBoundingClientRect();
+        this.trackStart = coordObj.left;
+        this.trackEnd = coordObj.right;
+        console.log(`track start: ${this.trackStart}`);
+        console.log(`track end: ${this.trackEnd}`);
+        console.log(`track width: ${this.trackWidth}`);
+    },
+
+    createThumbs(number) {
+        for (let i = 0; i < number; i++) {
+            let indexOfThumb = this.thumb.length;
+            document.getElementById(`slider${this.ID}`).innerHTML += `<div class="thumb" id="thumb${this.ID}${indexOfThumb}"></div>`;
+            this.thumb[indexOfThumb] = {
+                translate: 0,
+                translateCSS: undefined,
+                calcTranslateCSS() {
+                    this.translateCSS = `translate(${this.translate}%, 0px)`
+                },
+            };
+            this.thumb[i].calcTranslateCSS();
+        }
+    },
+
+    setThumb(index) { 
+        function setThisThumb (input) {
+            const singleMode = (typeof input === 'number');
+
+            let index = singleMode ? input : slider.thumb.indexOf(input);
+            let overlappedSectors = slider.sector.slice(0, index+1);
+            let offset = overlappedSectors.reduce((acc, next) => acc + next.scale, 0);
+            let currentElement = document.getElementById(`thumb${slider.ID}${index}`);
+            let currentThumb = singleMode ? slider.thumb[input] : input;
+
+            currentThumb.xAbsoluteDefault = currentElement.getBoundingClientRect().left;
+            currentThumb.xAbsolute = currentElement.getBoundingClientRect().left;
+            currentThumb.translate = slider.trackWidth*offset/slider.thumbWidth*100 - 50; 
+            currentThumb.calcTranslateCSS();
+            currentElement.setAttribute('style', `transform: ${slider.thumb[index].translateCSS}`);
+            console.log(`defaultThumb${index}: ${currentThumb.translate}`);
+        }
+
+        if (index === 'all') {
+            this.thumb.forEach((thumb) => {setThisThumb(thumb)});
+        } else if (typeof index === 'number') {
+            setThisThumb(index);
+        }
+    },
+
+    setSectors() {
+        for (let i = 0; i <= this.thumb.length; i++) {
+            document.getElementById(`track${this.ID}`).innerHTML += `<div class="sector" id="sector${this.ID}${i}"></div>`
+            this.sector[i] = {
+                scale: 0,
+                scaleCSS: undefined,
+                calcScaleCSS() {
+                    this.scaleCSS = `scale(${this.scale}, 1)`
+                },
+                translate: 0,
+                translateCSS: undefined,
+                calcTranslateCSS() {
+                    this.translateCSS = `translate(${this.translate}%, 0px)`
+                },
+            };
+            this.sector[i].calcScaleCSS();
+            this.sector[i].calcTranslateCSS();
+        }
+    },
+ 
+    calculateInitScale() {
+        const sectorsArr = this.sector;
+        let initScale = 1 / (sectorsArr.length-2);
+        sectorsArr.forEach((sector) => {
+            let index = sectorsArr.indexOf(sector);
+            let sectorEl = document.getElementById(`sector${this.ID}${index}`);
+            if (sectorEl.id === `sector${this.ID}0` || sectorEl.id === `sector${this.ID}${sectorsArr.length-1}`){
+                sector.scale = 0;
+                sector.calcScaleCSS();
+
+                sectorEl.setAttribute('style', `transform: ${sectorsArr[index].scaleCSS} ${sectorsArr[index].translateCSS}`)
+            } else {
+                sector.scale = initScale;
+                sector.calcScaleCSS();
+                sectorEl.setAttribute('style', `transform: ${sectorsArr[index].scaleCSS} ${sectorsArr[index].translateCSS}`)
+            }
+            
+        });
+    },
+
+
+    calculateInitTranslate() {
+        const sectorsArr = this.sector;
+        let leftSScalesSum = 0;
+        sectorsArr.forEach((sector) => {
+            let index = sectorsArr.indexOf(sector);
+            let sectorEl = document.getElementById(`sector${this.ID}${index}`);
+            let sectorScale = sector.scale;
+            sector.translate = ((index === sectorsArr.length)? 1 : leftSScalesSum) / ((sectorScale) ? sectorScale : 1) *100;
+
+            leftSScalesSum += sectorScale;
+            sectorsArr[index].calcTranslateCSS();
+            sectorEl.setAttribute('style', `transform: ${sectorsArr[index].scaleCSS} ${sectorsArr[index].translateCSS}`)
+        });
+    },
+
+    sector: [],
+
+    thumb: [],
 
 //at that moment take x as difference between start position of cursor and his current position ((start - current))
-    moveThumbMin(offset) {
-            newX = this.thumbMin.xThumbAbsolute + offset
-            if (13 < newX < 100) {
-                this.thumbMin.xThumbAbsolute = newX;
+    moveThumb(thumbIndex, offset) {
+            let thumbsArr =  this.thumb;
+            let currentX = thumbsArr[thumbIndex].xAbsolute;
+            let minLimit = thumbsArr[thumbIndex].xAbsoluteDefault;
+            let maxLimit = thumbsArr[thumbIndex + 1].xAbsolute; 
+            let newX = currentX + offset;
+
+            if (minLimit < newX < maxLimit) {
+                thumbsArr.xAbsolute = newX;
             }
             const minDefaultPosition = this.thumbMin.positionDefault;
             const maxDefaultPosition = this.thumbMax.positionDefault;
@@ -82,42 +246,9 @@ const slider = {
             sectorTwo.setAttribute('style', `transform: scale(${newMainScale}, 1) translate(${newMainTranslate}%, 0px)`);
             thumbMinEl.setAttribute('style', `translate: ${newThumbMinPosition}% 0%`);
     },
-    
-    moveThumbMax(x) {
-        let offset = Math.abs(x);
-
-        if(x < 0) {
-            this.emptyMax.scale += offset/100;
-            this.mainSector.scale -= offset/100;
-            this.thumbMax.positionCurrent -= offset*10;
-            if ((this.thumbMax.positionCurrent > this.thumbMin.positionDefault) && (this.thumbMax.positionCurrent <= this.thumbMin.positionCurrent)) {
-                this.emptyMax.scale = this.mainSector.scale - this.emptyMax.scale - 0.01;
-                this.mainSector.scale = 0.01;
-                this.thumbMin.positionCurrent = this.thumbMin.positionCurrent + 10;
-
-            }else if(this.thumbMax.positionCurrent <= this.thumbMin.positionDefault) {
-                this.emptyMax.scale = 0.99;
-                this.mainSector.scale = 0.01;
-                this.thumbMax.positionCurrent = -40;
-            }
-        } else if(x > 0){
-            this.emptyMax.scale -= offset/100;
-            this.mainSector.scale += offset/100;
-            this.thumbMax.positionCurrent += offset*10;
-            if(this.thumbMax.positionCurrent < this.thumbMax.positionDefault){
-                this.emptyMax.scale = 0;
-                this.mainSector.scale -= this.emptyMin.scale;
-                this.mainSector.translate = 0;
-                this.thumbMin.positionCurrent = 950;
-            }
-        }
-        sectorTwo.setAttribute('style', `transform: scale(${this.mainSector.scale}, 1) translate(${this.mainSector.translate}%, 0px)`);
-        sectorThree.setAttribute('style', `transform: scale(${this.emptyMax.scale}, 1) translate(0%, 0px)`);
-        thumbMaxEl.setAttribute('style', `translate: ${this.thumbMax.positionCurrent}% 0%`);
-    },
 }
 
-slider.thumbMax = {
+/*slider.thumbMax = {
     // 10 is thumb width and 50 is 50% of thumb - needed in order to place thumb center on chosen value. 
     positionDefault: (slider.width - 10) / 10 * 100 + 50, 
     initPos() {return this.positionDefault},
@@ -132,12 +263,15 @@ thumbMinEl.addEventListener('mousedown', (e) => {
     onThumb = true;
     let thumbCoordinates = e.target.getBoundingClientRect();
     slider.thumbMin.xThumbAbsolute = thumbCoordinates.left + thumbCoordinates.width/2;
+    e.preventDefault;
 })
 window.addEventListener('mousemove', (e) => {
     if (onThumb) {
-        slider.moveThumbMin((e.clientX-slider.thumbMin.xThumbAbsolute));
+        slider.moveThumb((e.clientX-slider.thumbMin.xThumbAbsolute));
     }
+    e.preventDefault;
 });
-window.addEventListener('mouseup', () => {
+window.addEventListener('mouseup', (e) => {
     onThumb = false;
-});
+    e.preventDefault;
+}); */
